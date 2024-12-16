571a495d4a31f69aabf6bb7da4408e8ee17a7911
Tyrsonnur <andresf199810@hotmail.com>
2024-12-16 08:25:40 -0500

    Actualizacion 16-12-2024 8:25 a.m

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,3 +18,20 @@ app.listen(port, (error) => {
 });
 
 
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "config.env" });
+
+const connectDB = async () => {
+
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+
+        })
+    } catch (error) {
+        console.log("BD conectada")
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
+
