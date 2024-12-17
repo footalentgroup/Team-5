f9e1fbdee653755e1e0c4df7e00aedf835005cc7
Robariza <robinsonariza2001@gmail.com>
2024-12-17 15:48:36 -0500

    [update] configuración app. endpoints, multer

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,57 +1,54 @@
-import express from 'express';  // Framework para crear el servidor web en Node.js
-import passport from 'passport';  // Passport para gestionar la autenticación
-import session from 'express-session';  // Middleware para gestionar sesiones
-import dotenv from 'dotenv';  // dotenv para cargar las variables de entorno
-import cors from 'cors';  // Middleware para habilitar CORS
-import authRoutes from './routes/auth-discord.routes.js';  // Rutas de autenticación con Discord
-import userRoutes from './routes/user.routes.js';  // Rutas relacionadas con usuarios
-import './passport.js';  // Importar la configuración de Passport
-import mongoDB from './config/db.js';  // Importar la función para conectar a MongoDB
+import express from 'express';
+import passport from 'passport';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import userRoutes from './routes/user.routes.js'
+import authDiscordRoutes from './routes/auth-discord.routes.js';
+import mongoDB from './config/db.js';
+import './passport.js';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-// Cargar las variables de entorno desde el archivo .env
-dotenv.config();  // Carga las variables de entorno para acceder a las configuraciones seguras
+// Configura __dirname para ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// Crear una instancia de la aplicación Express
+// Cargar las variables de entorno
+dotenv.config();
+
 const app = express();
 
-// Middleware para convertir las peticiones en formato JSON
-app.use(express.json());  // Permite recibir peticiones en formato JSON (para POST, PUT, etc.)
+app.use(express.json());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+app.use(session({
+  secret: 'tu_clave_secreta',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+}));
 
-// Middleware para permitir peticiones de otros dominios (CORS)
-app.use(cors());  // Permite que el frontend (por ejemplo, Angular) realice peticiones al backend
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Configuración de la sesión
-app.use(
-  session({
-    secret: 'tu_clave_secreta',  // Clave secreta para firmar las sesiones, debe ser privada
-    resave: false,  // No vuelve a guardar la sesión si no ha sido modificada
-    saveUninitialized: false,  // No guarda sesiones nuevas si no han sido inicializadas
-    cookie: {
-      secure: process.env.NODE_ENV === 'production',  // Asegura que las cookies solo se usen en producción
-      maxAge: 24 * 60 * 60 * 1000,  // La cookie expirará después de 24 horas
-    },
-  })
-);
+// Sirve archivos estáticos desde la carpeta uploads
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Inicializar Passport para la autenticación
-app.use(passport.initialize());  // Inicializa Passport en la aplicación Express
-app.use(passport.session());  // Establece que Passport utilice las sesiones para almacenar la información del usuario
+// Rutas
+app.use('/api/users', userRoutes);
+app.use('/api/auth/discord', authDiscordRoutes);
 
-// Definir las rutas de la API de usuario
-app.use('/api/auth', userRoutes);  // Rutas relacionadas con la autenticación y gestión de usuarios
+// Conexión a la base de datos
+mongoDB();
 
-// Definir las rutas de autenticación con Discord
-app.use('/auth/discord', authRoutes);  // Rutas que gestionan la autenticación a través de Discord
-
-// Conectar a la base de datos MongoDB
-mongoDB();  // Llama a la función de conexión de MongoDB
-
-// Iniciar el servidor en el puerto configurado en las variables de entorno
-const port = process.env.PORT || 6000;  // Utiliza el puerto configurado en las variables de entorno o por defecto 6000
-app.listen(port, (error) => {
-  if (error) {
-    console.error(`Error al iniciar el servidor: ${error.message}`);
-  } else {
-    console.log(`El servidor se está ejecutando en el puerto: ${port}`);  // Muestra el mensaje indicando el puerto donde el servidor está en ejecución
-  }
+// Puerto de la aplicación
+const port = process.env.PORT || 6000;
+app.listen(port, () => {
+  console.log(`El servidor se está ejecutando en el puerto: ${port}`);
 });
