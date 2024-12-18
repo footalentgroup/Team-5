4b9c7cf5f2b53cbe5e423e4ecc6d63080710c3e4
Stiven <stivenpoveda30sistema@gmail.com>
2024-12-17 19:20:43 -0500

    delete eventos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
 
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
