2f616ca7c466339a12ed5ddd1dab9acbb6b2a85f
Robariza <robinsonariza2001@gmail.com>
2025-01-08 10:42:14 -0500

    [update] Eliminación de archivos piloto

--- backend/src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const { isNotAuthorized } = require('../utils/auth')
-const router = Router()
-
-router.get('/', isNotAuthorized ,(req, res) => {
-  res.render("home")
-})
-
-module.exports = router
