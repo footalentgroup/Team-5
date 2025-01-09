b61998b8d2790105b2ee508c117aca9d1186abc7
Robariza <robinsonariza2001@gmail.com>
2025-01-09 11:58:27 -0500

    [update] configuración cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,10 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+app.use(cors());
 app.use(session({
   secret: 'tu_clave_secreta',
   resave: false,
