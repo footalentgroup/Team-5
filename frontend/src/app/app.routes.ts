a6f3764161f71facde3d07320a1f4db103da5271
Robariza <robinsonariza2001@gmail.com>
2025-01-11 14:11:17 -0500

    [update] user-profile component, assets, css, html y routes.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { authGuard } from './guards/auth.guard';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'verify-email', component: VerifyEmailComponent },
-  { path: 'landing', component: LandingComponent }
+  { path: 'landing', component: LandingComponent },
+  { path: 'user-profile', component: UserProfileComponent }
 ];
