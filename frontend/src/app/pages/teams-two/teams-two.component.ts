fe67422447f4bcddcac302fe9360119507c819b5
Angelo Gonzalez <angellog9714@gmail.com>
2025-01-12 18:49:54 -0500

    Maquetación pagina teams destoks 2

---
+++ frontend/src/app/pages/teams-two/teams-two.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teams-two',
+  standalone: true,
+  imports: [],
+  templateUrl: './teams-two.component.html',
+  styleUrl: './teams-two.component.css'
+})
+export class TeamsTwoComponent {
+
+}
