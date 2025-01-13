6d095ecb9b9232837344b92acdde4ec5fab63605
Angelo Gonzalez <angellog9714@gmail.com>
2025-01-13 13:31:19 -0500

    Avance destoks 2 equipos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,19 +7,21 @@ import { VerifyEmailComponent } from './components/verify-email/verify-email.com
 import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TeamsComponent } from './pages/teams/teams.component';
+import { TeamsTwoComponent } from './pages/teams-two/teams-two.component';
 
 // import { NgModel } from '@angular/forms';
 // import { NgModule } from '@angular/core';
 
 
 export const routes: Routes = [
-  // { path: '', redirectTo: '/home', pathMatch: 'full' }, //Ruta prederteminada
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, //Ruta prederteminada
   // { path: '**', redirectTo: '/home', pathMatch: 'full' }, // Comodin para rutas no encontradas "pendiente modificar (error)"
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'verify-email', component: VerifyEmailComponent },
   { path: 'dashboard', component: DashboardComponent, title: 'dashboard'},
-  { path: 'teams', component: TeamsComponent}
+  { path: 'teams', component: TeamsComponent},
+  { path: 'teams-two', component: TeamsTwoComponent}
 ];
 
 // @NgModule({
