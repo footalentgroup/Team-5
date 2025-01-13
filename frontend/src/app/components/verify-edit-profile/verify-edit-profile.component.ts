f72dd4fa3f21d6896de2b4cb8de5fa622e2f17da
Robariza <robinsonariza2001@gmail.com>
2025-01-13 15:06:38 -0500

    [update] verify-edit-profile component, assets, css, html.

---
+++ frontend/src/app/components/verify-edit-profile/verify-edit-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-verify-edit-profile',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './verify-edit-profile.component.html',
+  styleUrl: './verify-edit-profile.component.css'
+})
+export class VerifyEditProfileComponent {
+
+}
