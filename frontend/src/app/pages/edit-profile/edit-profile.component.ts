d2035994494d114cc144cb4026d3890a6a373fe9
Robariza <robinsonariza2001@gmail.com>
2025-01-14 13:08:50 -0500

    [update] verify component, responsive, routerlink, share button.

--- frontend/src/app/pages/edit-profile/edit-profile.component.ts
+++ frontend/src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.css'
 })
