c42b5c854172c2fa19ab0eaf73faf6ae83dfefb9
Robariza <robinsonariza2001@gmail.com>
2025-01-15 17:11:03 -0500

    [update] search service.

---
+++ frontend/src/app/services/search.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private apiUrl = 'http://localhost:3000/api/search';
+
+  constructor(private http: HttpClient) {}
+
+  search(query: string, types: string[]): Observable<any> {
+    const params = new HttpParams()
+      .set('query', query)
+      .set('types', types.join(',')); // Concatena los tipos seleccionados
+
+    return this.http.get<any>(this.apiUrl, { params });
+  }
+}
