520e38a20398f09670513a6c102d22c6bc1ec125
Robariza <robinsonariza2001@gmail.com>
2025-01-15 17:11:36 -0500

    [update] landing component, html, css, ts. Implementación search component

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SearchComponent } from "../../components/search/search.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SearchComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
