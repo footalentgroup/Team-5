51071f1bbca6768eda074ffbbbe77fe43cf97a7c
Robariza <robinsonariza2001@gmail.com>
2025-01-16 18:19:16 -0500

    [update] dashboard component

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from "../../components/search/search.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
