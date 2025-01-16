92d0e346f0d3918a9ce8a5382461b70e4127ff2c
Angelo Gonzalez <angellog9714@gmail.com>
2025-01-16 01:34:50 -0500

    Avance responsive de pagina equipos

--- frontend/src/app/pages/teams-two/teams-two.component.ts
+++ frontend/src/app/pages/teams-two/teams-two.component.ts
@@ -1,13 +1,7 @@
 import { Component, importProvidersFrom } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faAward } from '@fortawesome/free-solid-svg-icons'; 
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
-import { faShareNodes } from '@fortawesome/free-solid-svg-icons';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faAward, faUser, faCircle, faPeopleGroup, faTrophy, faShareNodes, faBell } from '@fortawesome/free-solid-svg-icons'; 
 
 @Component({
   selector: 'app-teams-two',
