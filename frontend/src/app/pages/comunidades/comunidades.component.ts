723f73461a8a95defe3193e8c97753f15c95e8b8
Celussky <gabri.bernal@gmail.com>
2025-01-15 20:41:29 -0500

    comunidades principal sin conectar al backend

--- frontend/src/app/pages/comunidades/comunidades.component.ts
+++ frontend/src/app/pages/comunidades/comunidades.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-comunidades',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
   templateUrl: './comunidades.component.html',
   styleUrl: './comunidades.component.css'
 })
 export class ComunidadesComponent {
+  faPeopleGroup = faPeopleGroup
+  faPen = faPen
+  faAddressCard = faAddressCard
 }
