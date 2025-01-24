6cd20ac30aecbae42b2d9b216c195e954fcde70d
Angelo Gonzalez <angellog9714@gmail.com>
2025-01-23 22:12:00 -0500

    actualizacion

--- frontend/src/app/pages/teams-three/teams-three.component.ts
+++ frontend/src/app/pages/teams-three/teams-three.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TeamsTwoComponent } from '../teams-two/teams-two.component';
+import { TeamsComponent } from '../teams/teams.component';
+
+
 // Importaciones de los iconos de font Awesome
 import { faAddressCard, faUserPlus, faArrowDown, faPen, faCrown,faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-teams-three',
   standalone: true,
-  imports: [RouterLink, FontAwesomeModule, TeamsTwoComponent],
+  imports: [RouterLink, FontAwesomeModule, TeamsTwoComponent, TeamsComponent],
   templateUrl: './teams-three.component.html',
   styleUrl: './teams-three.component.css'
 })
