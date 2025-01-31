a0b910f7bcab64c9ccb74ba9c32cb8b31db748a8
Robariza <robinsonariza2001@gmail.com>
2025-01-30 19:03:15 -0500

    [update] landing

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SearchComponent } from "../../components/search/search.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink, SearchComponent],
+  imports: [RouterLink, SearchComponent, CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  isHovered: boolean = false;
 
+  onMouseEnter() {
+    this.isHovered = true;
+  }
+
+  onMouseLeave() {
+    this.isHovered = false;
+  }
 }
